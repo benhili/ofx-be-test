9f3aa24008e2fb03cd79b15ffeb8129a751efeaf
Benjamin Hili <ben.hili1994@gmail.com>
2025-08-07 11:29:38 +1000

    Add implementation and tests for getPayment handler

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -1,5 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
+import { getPayment } from './lib/payments';
+import { buildResponse } from './lib/apigateway';
+
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    throw Error('Implement me!');
+    const paymentId = event.pathParameters?.id
+    if (!paymentId) {
+        return buildResponse(400, {message: "No payment ID provided"})
+    }
+
+    const payment = await getPayment(paymentId)
+    if (!payment) {
+        return buildResponse(422, {message: `No payment found for payment ID ${paymentId}`})
+    }
+    return buildResponse(200, payment)
 };
--- test/getPayment.test.ts
+++ test/getPayment.test.ts
@@ -24,6 +24,30 @@ describe('When the user requests the records for a specific payment', () => {
 
         expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
     });
+    it("Informs the caller when a payment ID hasn't been provided", async () => {
+        const result = await handler({
+            pathParameters: {
+            },
+        } as unknown as APIGatewayProxyEvent);
+
+        expect(result.statusCode).toBe(400);
+        expect(JSON.parse(result.body)).toEqual({message: "No payment ID provided"});
+    });
+    it("Informs the caller when a payment wasn't found in dynamodb", async () => {
+        const paymentId = randomUUID();
+        const getPaymentMock = jest.spyOn(payments, 'getPayment').mockResolvedValueOnce(null);
+
+        const result = await handler({
+            pathParameters: {
+                id: paymentId,
+            },
+        } as unknown as APIGatewayProxyEvent);
+
+        expect(result.statusCode).toBe(422);
+        expect(JSON.parse(result.body)).toEqual({message: `No payment found for payment ID ${paymentId}`});
+
+        expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
+    });
 });
 
 afterEach(() => {
