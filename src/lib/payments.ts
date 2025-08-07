797928080f7798742bf0b0fbcb575592599d0502
Benjamin Hili <ben.hili1994@gmail.com>
2025-08-07 12:21:46 +1000

    Format payments with prettier, missed in prev commit

--- src/lib/payments.ts
+++ src/lib/payments.ts
@@ -1,34 +1,36 @@
-import { DocumentClient } from './dynamodb';
-import { GetCommand, PutCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { DocumentClient } from "./dynamodb";
+import { GetCommand, PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
-export const getPayment = async (paymentId: string): Promise<Payment | null> => {
-    const result = await DocumentClient.send(
-        new GetCommand({
-            TableName: 'Payments',
-            Key: { paymentId },
-        })
-    );
+export const getPayment = async (
+  paymentId: string
+): Promise<Payment | null> => {
+  const result = await DocumentClient.send(
+    new GetCommand({
+      TableName: "Payments",
+      Key: { paymentId },
+    })
+  );
 
-    return (result.Item as Payment) || null;
+  return (result.Item as Payment) || null;
 };
 
 export const listPayments = async (): Promise<Payment[]> => {
-    const result = await DocumentClient.send(
-        new ScanCommand({
-            TableName: 'Payments',
-        })
-    );
+  const result = await DocumentClient.send(
+    new ScanCommand({
+      TableName: "Payments",
+    })
+  );
 
-    return (result.Items as Payment[]) || [];
+  return (result.Items as Payment[]) || [];
 };
 
 export const createPayment = async (payment: Payment) => {
-    await DocumentClient.send(
-        new PutCommand({
-            TableName: 'Payments',
-            Item: payment,
-        })
-    );
+  await DocumentClient.send(
+    new PutCommand({
+      TableName: "Payments",
+      Item: payment,
+    })
+  );
 };
 
 // TODO: there's probably a library for this or you could just
