ca341720e7cd59160c6d7ed8cc5c31b034d8b66c
Benjamin Hili <ben.hili1994@gmail.com>
2025-08-07 12:05:40 +1000

    Add prettier and format project

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -1,17 +1,21 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-import { getPayment } from './lib/payments';
-import { buildResponse } from './lib/apigateway';
+import { getPayment } from "./lib/payments";
+import { buildResponse } from "./lib/apigateway";
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const paymentId = event.pathParameters?.id
-    if (!paymentId) {
-        return buildResponse(400, {message: "No payment ID provided"})
-    }
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const paymentId = event.pathParameters?.id;
+  if (!paymentId) {
+    return buildResponse(400, { message: "No payment ID provided" });
+  }
 
-    const payment = await getPayment(paymentId)
-    if (!payment) {
-        return buildResponse(422, {message: `No payment found for payment ID ${paymentId}`})
-    }
-    return buildResponse(200, payment)
+  const payment = await getPayment(paymentId);
+  if (!payment) {
+    return buildResponse(422, {
+      message: `No payment found for payment ID ${paymentId}`,
+    });
+  }
+  return buildResponse(200, payment);
 };
--- test/getPayment.test.ts
+++ test/getPayment.test.ts
@@ -1,55 +1,62 @@
-import * as payments from '../src/lib/payments';
-import { randomUUID } from 'crypto';
-import { handler } from '../src/getPayment';
-import { APIGatewayProxyEvent } from 'aws-lambda';
+import * as payments from "../src/lib/payments";
+import { randomUUID } from "crypto";
+import { handler } from "../src/getPayment";
+import { APIGatewayProxyEvent } from "aws-lambda";
 
-describe('When the user requests the records for a specific payment', () => {
-    it('Returns the payment matching their input parameter.', async () => {
-        const paymentId = randomUUID();
-        const mockPayment = {
-            id: paymentId,
-            currency: 'AUD',
-            amount: 2000,
-        };
-        const getPaymentMock = jest.spyOn(payments, 'getPayment').mockResolvedValueOnce(mockPayment);
+describe("When the user requests the records for a specific payment", () => {
+  it("Returns the payment matching their input parameter.", async () => {
+    const paymentId = randomUUID();
+    const mockPayment = {
+      id: paymentId,
+      currency: "AUD",
+      amount: 2000,
+    };
+    const getPaymentMock = jest
+      .spyOn(payments, "getPayment")
+      .mockResolvedValueOnce(mockPayment);
 
-        const result = await handler({
-            pathParameters: {
-                id: paymentId,
-            },
-        } as unknown as APIGatewayProxyEvent);
+    const result = await handler({
+      pathParameters: {
+        id: paymentId,
+      },
+    } as unknown as APIGatewayProxyEvent);
 
-        expect(result.statusCode).toBe(200);
-        expect(JSON.parse(result.body)).toEqual(mockPayment);
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.body)).toEqual(mockPayment);
 
-        expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
+    expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
+  });
+  it("Informs the caller when a payment ID hasn't been provided", async () => {
+    const result = await handler({
+      pathParameters: {},
+    } as unknown as APIGatewayProxyEvent);
+
+    expect(result.statusCode).toBe(400);
+    expect(JSON.parse(result.body)).toEqual({
+      message: "No payment ID provided",
     });
-    it("Informs the caller when a payment ID hasn't been provided", async () => {
-        const result = await handler({
-            pathParameters: {
-            },
-        } as unknown as APIGatewayProxyEvent);
+  });
+  it("Informs the caller when a payment wasn't found in dynamodb", async () => {
+    const paymentId = randomUUID();
+    const getPaymentMock = jest
+      .spyOn(payments, "getPayment")
+      .mockResolvedValueOnce(null);
 
-        expect(result.statusCode).toBe(400);
-        expect(JSON.parse(result.body)).toEqual({message: "No payment ID provided"});
+    const result = await handler({
+      pathParameters: {
+        id: paymentId,
+      },
+    } as unknown as APIGatewayProxyEvent);
+
+    expect(result.statusCode).toBe(422);
+    expect(JSON.parse(result.body)).toEqual({
+      message: `No payment found for payment ID ${paymentId}`,
     });
-    it("Informs the caller when a payment wasn't found in dynamodb", async () => {
-        const paymentId = randomUUID();
-        const getPaymentMock = jest.spyOn(payments, 'getPayment').mockResolvedValueOnce(null);
 
-        const result = await handler({
-            pathParameters: {
-                id: paymentId,
-            },
-        } as unknown as APIGatewayProxyEvent);
-
-        expect(result.statusCode).toBe(422);
-        expect(JSON.parse(result.body)).toEqual({message: `No payment found for payment ID ${paymentId}`});
-
-        expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
-    });
+    expect(getPaymentMock).toHaveBeenCalledWith(paymentId);
+  });
 });
 
 afterEach(() => {
-    jest.resetAllMocks();
+  jest.resetAllMocks();
 });
